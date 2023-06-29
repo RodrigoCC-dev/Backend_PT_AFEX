ab5c94cefd1d138db83c5c522b15ed3a387dac2f
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-06-29 18:13:39 -0400

    Models: Se agrega configuración de llaves foráneas entre modelos 'Video' y 'Description'

--- db/models/video.js
+++ db/models/video.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Video.hasMany(models.Description)
+      Video.hasMany(models.Description, {foreignKey: 'videoId'})
     }
   }
   Video.init({
